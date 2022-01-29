a1bb763a17d4c2f86f6f1848c3129a6d78cfa509
Ormary Montilla <ormary.montilla@gmail.com>
2022-01-29 20:51:32 -0300

    Mi proyecto final

---
+++ enviar.php
@@ -0,0 +1,27 @@
+<?php
+$destinatario = 'ormary.montilla@gmail.com';
+$name = $_POST['name'];
+$mail = $_POST['mail'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$message = "Este mensaje fue enviado por: " . $name . " \r\n";
+$message .= "Su e-mail es: " . $mail . " \r\n";
+$message .= "Teléfono de contacto: " . $phone . " \r\n";
+$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
+
+$para = 'ormary.montilla@gmail.com';
+$asunto = 'En breve te respondemos';
+
+mail($destinatario, $asunto, utf8_decode($message), $header);
+echo "<script>alert('correo enviado exitosamente')</script>";
+echo "<scripto>setTimeout(\"location.href='contacto.html'\",1000)</script>";
+
+header("Location:contacto.html");
+?>
